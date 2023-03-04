6463b330097d00ff173c65b079d59ce1061cc608
datowq <steven.andrew.lew@gmail.com>
2023-03-04 04:15:35 -0800

    fix: fixed some discrepancies with the spotifyauth merge

--- frontend/src/components/SpotifyLogin.jsx
+++ frontend/src/components/SpotifyLogin.jsx
@@ -1,11 +1,9 @@
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
-const URL = 'http://localhost:5000';
-
 const SpotifyLogin = () => {
     return (
-        <Link to='http://localhost:5000/spotify/login' className='hover:opacity-80 bg-gradient-to-r from-backgroundc-200 to-green-500 text-white px-4 py-2 rounded-md'>
+        <Link to='http://localhost:3001/spotify/login' className='hover:opacity-80 bg-gradient-to-r from-backgroundc-200 to-green-500 text-white px-4 py-2 rounded-md'>
             spotify
         </Link> 
     )
