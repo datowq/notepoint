d17fd4187761f3ddaae61ffa72508b6139bd9b66
Michelle Lee <michellehlee07@gmail.com>
2023-03-03 17:58:06 -0800

    Minor format changes

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -2,7 +2,9 @@ import { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import './About.css'
 
+
 function About () {
+    
     return (
         <div className="Main">
             <div className="Sections">
