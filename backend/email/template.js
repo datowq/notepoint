33578c5c03d726975de335b6cd6bd32f46037b71
datowq <steven.andrew.lew@gmail.com>
2023-03-09 17:43:43 -0800

    fix: fixed environment variables for vercel deployment

--- backend/email/template.js
+++ backend/email/template.js
@@ -1,23 +1,27 @@
+require('dotenv').config();
+
+const frontend_uri = process.env.FRONTEND_URL;
+
 module.exports = {
 
   confirm: id => ({
     subject: 'NotePoint Email Adress Verification',
     html: `
-      <a href='http://localhost:5173/verify/${id}'>
+      <a href='${frontend_uri}/verify/${id}'>
         Click to confirm email
       </a>
     `,      
-    text: `Copy and paste this link: http://localhost:5173/verify/${id}`
+    text: `Copy and paste this link: ${frontend_uri}/verify/${id}`
   }),
 
   recover: id => ({
     subject: 'Recover your NotePoint account', 
     html: `
-      <a href='http://localhost:5173/recover/${id}'>
+      <a href='${frontend_uri}/recover/${id}'>
         Click to create a new password for your account
       </a>
     `, 
-    text: `Copy and paste this link: http://localhost:5173/recover/${id}`
+    text: `Copy and paste this link: ${frontend_uri}/recover/${id}`
   })
   
 }
